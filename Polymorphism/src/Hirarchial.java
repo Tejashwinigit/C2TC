bc8697b4c71b8fc40a85c3ac5baf8b48c095612b
admin <admin@teju2629>
2022-02-10 14:08:03 +0530

    Hirarchieal inheritence

---
+++ Polymorphism/src/Hirarchial.java
@@ -0,0 +1,43 @@
+
+
+	class N
+	{
+	   public void methodA()
+	   {
+	      System.out.println("method of Class A");
+	   }
+	}
+	class B extends N
+	{
+	   public void methodB()
+	   {
+	      System.out.println("method of Class B");
+	   }
+	}
+	class C extends N
+	{
+	  public void methodC()
+	  {
+	     System.out.println("method of Class C");
+	  }
+	}
+	class D extends N
+	{
+	  public void methodD()
+	  {
+	     System.out.println("method of Class D");
+	  }
+	}
+	class Hirarchial
+	{
+	  public static void main(String args[])
+	  {
+	     B obj1 = new B();
+	     C obj2 = new C();
+	     D obj3 = new D();
+	     //All classes can access the method of class A
+	     obj1.methodA();
+	     obj2.methodA();
+	     obj3.methodA();
+	  }
+}
