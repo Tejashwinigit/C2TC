058640737a77b9ffa35e8eb30b15df1212b0ec49
admin <admin@teju2629>
2022-02-10 13:13:15 +0530

    Public access modifier

---
+++ Accessmodifier/src/Public.java
@@ -0,0 +1,14 @@
+ class B{  
+public void msg(){System.out.println("Hello");}  
+}  
+//save by B.java  
+  
+ 
+
+  
+class Public{  
+  public static void main(String args[]){  
+   B obj = new B();  
+   obj.msg();  
+  }  
+}  
