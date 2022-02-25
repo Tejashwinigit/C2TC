176248ebf7339fea64b96ce60d7cd4fb8f6bc254
admin <admin@teju2629>
2022-02-25 16:32:20 +0530

    linked list example program

---
+++ Linkedlist/src/GFG.java
@@ -0,0 +1,15 @@
+import java.util.LinkedList;
+
+public class GFG {
+  public static void main(String[] args){
+
+    
+    LinkedList<String> languages = new LinkedList<>();
+
+    // Add elements to LinkedList
+    languages.add("Java");
+    languages.add("Python");
+    languages.add("JavaScript");
+    System.out.println("LinkedList: " + languages);
+  }
+}
