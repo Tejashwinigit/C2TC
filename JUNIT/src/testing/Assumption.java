f471e70f7a8ad9ed347f632bc5baf862f790be51
admin <tejashwini2612@gmail.com>
2022-03-08 14:00:13 +0530

    Assumption

---
+++ JUNIT/src/testing/Assumption.java
@@ -0,0 +1,30 @@
+
+
+
+
+package testing;
+
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.Test;
+
+public class Assumption {
+		@Test
+	    void testOnDev() 
+		{
+			System.setProperty("ENV", "DEV");  // TRUE
+	        Assumptions.assumeTrue("DEV".equals(System.getProperty("ENV"))); // TRUE
+	        //remainder of test will proceed
+	    }
+		
+		@Test
+	    void testOnProd() 
+		{
+			System.setProperty("ENV", "PROD");// T
+	        Assumptions.assumingThat("DEV".equals(System.getProperty("ENV")), Assumption::message); // T
+	        //remainder of test will be aborted
+	    }
+		
+		private static String message () {
+			return "Today's TEST Execution Failed :: ";
+		}
+}
